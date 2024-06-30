6f7fc6a7e7ed1a31e53f9943c7c936d8bc299982
FatihYigit35 <izmirsoftware.com.tr@gmail.com>
2024-06-30 18:35:52 +0300

    bottom navigation ve home fragment eklendi

---
+++ app/src/main/java/com/izmirsoftware/petsmatch/viewmodel/home/HomeViewModel.kt
@@ -0,0 +1,76 @@
+package com.izmirsoftware.petsmatch.viewmodel.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.izmirsoftware.petsmatch.model.Comments
+import com.izmirsoftware.petsmatch.model.Location
+import com.izmirsoftware.petsmatch.model.Owner
+import com.izmirsoftware.petsmatch.model.Pet
+import com.izmirsoftware.petsmatch.model.PetCardModel
+import com.izmirsoftware.petsmatch.model.PetPost
+import com.izmirsoftware.petsmatch.viewmodel.BaseViewModel
+import java.util.*
+
+class HomeViewModel : BaseViewModel() {
+    val petCardModel: LiveData<List<PetCardModel>> = MutableLiveData()
+
+    fun createPetCardModels() {
+        val petPost = PetPost()
+        petPost.title = "Çok sevimli kedi"
+        petPost.description = "Kedimiz çok sakindir, saldırganlık yapmaz, tuvalet eğitimi var."
+        petPost.location = Location(
+            city = "İzmir",
+            district = "Torbalı"
+        )
+        val currentTime = System.currentTimeMillis()
+        petPost.date = Date(currentTime)
+
+        val pet = Pet()
+        pet.profileImage = "https://cdn1.ntv.com.tr/gorsel/xXM8GccvjkmZxggiDVHP5g.jpg"
+
+        val owner = Owner()
+        owner.comments = listOf(
+            Comments(
+                rating = 4.8
+            )
+        )
+
+        petCardModel.mutable.value = listOf(
+            PetCardModel(
+                petPost = petPost,
+                pet = pet,
+                owner = owner
+            ),
+            PetCardModel(
+                petPost = petPost,
+                pet = pet,
+                owner = owner
+            ),
+            PetCardModel(
+                petPost = petPost,
+                pet = pet,
+                owner = owner
+            ),
+            PetCardModel(
+                petPost = petPost,
+                pet = pet,
+                owner = owner
+            ),
+            PetCardModel(
+                petPost = petPost,
+                pet = pet,
+                owner = owner
+            ),
+            PetCardModel(
+                petPost = petPost,
+                pet = pet,
+                owner = owner
+            ),
+            PetCardModel(
+                petPost = petPost,
+                pet = pet,
+                owner = owner
+            )
+        )
+    }
+}
